da10a9a8527e81755970863ea6a4828ccbc7405e
0xyshv <writetoyshv@gmail.com>
2024-07-08 15:14:13 +0530

    feat: add providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
+import { ConnectKitButton } from "connectkit";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Photon
+      <ConnectKitButton />
     </main>
   );
 }
