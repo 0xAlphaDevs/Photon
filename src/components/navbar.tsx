9d75615d2d72d313a8879a856cd4e5184ce835e8
0xyshv <writetoyshv@gmail.com>
2024-07-12 10:55:16 +0530

    fix: navbar scroll

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC<NavbarProps> = ({ links }) => {
   const pathname = usePathname();
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid fixed top-0 left-0 min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
