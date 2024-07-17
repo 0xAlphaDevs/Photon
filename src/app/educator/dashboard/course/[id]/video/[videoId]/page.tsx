12e181231a8ca94f4a0f2ccdb754d9d0c814c823
0xyshv <writetoyshv@gmail.com>
2024-07-17 18:48:19 +0530

    chore: add back routing to video

--- src/app/educator/dashboard/course/[id]/video/[videoId]/page.tsx
+++ src/app/educator/dashboard/course/[id]/video/[videoId]/page.tsx
@@ -1,8 +1,28 @@
+"use client"
+
 import React from 'react'
+import { MoveLeftIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const VideoIdPage = () => {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
-    <div>VideoIdPage</div>
+    <div className='py-8'>
+      <div className='flex items-center gap-4'>
+        <div
+          className="h-6 w-6 cursor-pointer mr-2"
+          onClick={handleBackClick}
+        >
+          <MoveLeftIcon className='h-6 w-6' />
+        </div>
+        <p className='text-3xl font-medium'>Video Id </p>
+      </div>
+    </div>
   )
 }
 
