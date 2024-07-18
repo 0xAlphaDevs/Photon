2979b20b370d6089c37f89e03d663e31dbf418a3
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-07-18 09:59:08 +0530

    chore: add updated abis

--- src/lib/abi/PhotonCourseAbi.ts
+++ src/lib/abi/PhotonCourseAbi.ts
@@ -1,4 +1,4 @@
-export const courseNftAbi = [
+export const PhotonCourseAbi = [
   {
     inputs: [
       {
---
+++ src/lib/abi/PhotonCourseFactoryAbi.ts
@@ -0,0 +1,94 @@
+export const PhotonCourseFactoryAbi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "_course",
+        type: "address",
+      },
+    ],
+    name: "CourseCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "symbol",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+    ],
+    name: "createCourse",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllCourses",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllCoursesPurchasedByLearner",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getCoursesCreatedByEducator",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
